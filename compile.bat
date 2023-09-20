9e7896bf847a9e3b320c3a2fe756ec98b7a4a2c4
onelivesleft <iainking@gmail.com>
2023-09-20 17:47:29 +0100

    Fix save_all

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
 @echo off
 
 jai first.jai
+rem copy /Y build_debug\focus_debug.exe .
+rem copy /Y build_debug\focus_debug.pdb .
--- dev.bat
+++ dev.bat
@@ -1,5 +1,5 @@
 @echo off
 
 jai first.jai
-copy build_debug\focus_debug.exe focus_dev.exe
-copy build_debug\focus_debug.pdb focus_dev.pdb
+copy /Y build_debug\focus_debug.exe focus_dev.exe
+copy /Y build_debug\focus_debug.pdb focus_dev.pdb
